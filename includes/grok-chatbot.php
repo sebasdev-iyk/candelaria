919e82dc51d0f889d2569b283cfd17639676543e
sebastian <p.sebastian.bn@gmail.com>
2026-01-23 19:15:12 -0500

    cahs

--- includes/grok-chatbot.php
+++ includes/grok-chatbot.php
@@ -1,4 +1,5 @@
 <!-- Grok Chatbot Widget -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <div id="candelaria-bot-widget" class="candelaria-bot-widget">
     <!-- Header -->
     <div class="chat-header">
